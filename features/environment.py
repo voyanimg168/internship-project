03e17837a3b3d0b514f1c95bbfe57a801e9a7d0c
Minru Gan <minrugan@gmail.com>
2025-04-24 21:35:38 -0600

    environment crossbrowser headless

--- features/environment.py
+++ features/environment.py
@@ -2,30 +2,85 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.wait import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
-# from webdriver_manager.firefox import GeckoDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+
 from app.application import Application
 from support.logger import logger
 
+# Command to run tests with Allure & Behave:
+# behave -f allure_behave.formatter:AllureFormatter -o test_results/ features/tests/target_search.feature
 
-def browser_init(context):
+
+def browser_init(context, scenario_name):
     """
     :param context: Behave context
     """
-    driver_path = ChromeDriverManager().install()
+    # driver_path = ChromeDriverManager().install()
+    # service = Service(driver_path)
+    # context.driver = webdriver.Chrome(service=service)
+
+    driver_path = GeckoDriverManager().install()
     service = Service(driver_path)
-    context.driver = webdriver.Chrome(service=service)
+    context.driver = webdriver.Firefox(service=service)
+
+    # context.driver = webdriver.Safari()  #driver already built into Safari
+
+    #Headless Mode
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('headless')
+    # service = Service(ChromeDriverManager().install())
+    # context.driver = webdriver.Chrome(
+    #   options=options,
+    #   service=service
+    # )
+
+    options = webdriver.FirefoxOptions()
+    options.add_argument('headless')
+    service = Service(GeckoDriverManager().install())
+    context.driver = webdriver.Firefox(
+      options=options,
+      service=service
+    )
+
+    #Register for BrowserStack, then grab from https://www.browserstack.com/accounts/settings
+    # bs_user =''
+    # bs_key =''
+    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    #
+    # options = Options()
+    # bstack_options = {
+    #     "os" : "Windows",
+    #     "osVersion" : "11",
+    #     "browserName" : 'edge',
+    #     "sessionName" : scenario_name,
+    # }
+    # options.set_capability('bstack:options', bstack_options)
+    # context.driver = webdriver.Remote(command_executor=url, options=options)
+
+    # bs_user=''
+    # bs_key =''
+    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    #
+    # options = Options()
+    # bstack_options = {
+    #     "os" : "Windows",
+    #     "osVersion" : "11",
+    #     "browserName" : 'Firefox',
+    #     "sessionName" : scenario_name,
+    # }
+    # options.set_capability('bstack:options', bstack_options)
+    # context.driver = webdriver.Remote(command_executor=url, options=options)
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4) #only shows up in environment.py file
     context.driver.wait = WebDriverWait(context.driver, 10)
     context.app = Application(context.driver)
 
-
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
     logger.info(f'\nStarted scenario: {scenario.name}')
-    browser_init(context)
+    browser_init(context, scenario.name)
 
 def before_step(context, step):
     logger.info(f'Started step: {step}')
@@ -44,3 +99,4 @@ def after_scenario(context, feature):
     #     for log_entry in browser_logs:
     #         log_file.write(f"{log_entry['level']}) - {log_entry['timestamp']} - {log_entry['message']}\n")
     # print("Browser logs saved to browser_logs.txt")
+
